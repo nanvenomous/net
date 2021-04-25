384b9e2cce44505f8427837e80ce198083f0b971
Matthew Garelli <mrgarelli@gmail.com>
2021-04-25 14:07:41 -0600

    MG | added routes

--- system/system.go
+++ system/system.go
@@ -28,6 +28,20 @@ func TestWifi() error {
 	return nil
 }
 
+func ShowRouteInfo() error {
+	cmd := exec.Command("ip", "route")
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func ShowInterfaceInfo() error {
 	cmd := exec.Command("ip", "addr")
 
