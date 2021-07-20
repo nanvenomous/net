cc78eb2e0afbcc1a5e77d7b8b6b8943371772b76
Matthew Garelli <mrgarelli@gmail.com>
2021-07-20 07:07:29 -0600

    MG | makefile now zsh by default test command has help

--- cmd/test.go
+++ cmd/test.go
@@ -23,13 +23,8 @@ import (
 // testCmd represents the test command
 var testCmd = &cobra.Command{
 	Use:   "test",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "pings google once",
+	Long:  `pings google once`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		err := system.TestWifi()
 		if err != nil {
