25f2b7e48bc7d9154030d4e01d02a6456b167d6b
Matthew Garelli <mrgarelli@gmail.com>
2021-07-26 00:09:26 -0700

    MG | updated package help message with general description

--- cmd/root.go
+++ cmd/root.go
@@ -35,13 +35,9 @@ var (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "net",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Manages network connections",
+	Long: `Manages network connections
+uses iwctl under the hood`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
